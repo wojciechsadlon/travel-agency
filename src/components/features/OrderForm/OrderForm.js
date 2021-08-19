c66e4f938c152df2d38fc7ec9919cccf91295a1f
Wojciech Sadłoń <wojciechsadlon@gmail.com>
2021-08-19 17:49:09 +0200

    error with server config

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -5,6 +5,38 @@ import { Row, Col } from 'react-flexbox-grid';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button';
+import settings from '../../../data/settings';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = ({tripCost, options, setOrderOption}) => (
   <Row>
@@ -15,6 +47,7 @@ const OrderForm = ({tripCost, options, setOrderOption}) => (
         </Col>
       ))}
       <OrderSummary cost={tripCost} options={options}/>
+      <Button onClick={() => sendOrder(options, tripCost)}>Order now!</Button>
     </Col>
   </Row>
 );
