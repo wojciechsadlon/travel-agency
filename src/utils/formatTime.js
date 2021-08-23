d9ca776020c30cc61aac07ad203be85cc64022c6
Wojciech Sadłoń <wojciechsadlon@gmail.com>
2021-08-23 19:16:20 +0200

    added formatTime func and tests

--- src/utils/formatTime.js
+++ src/utils/formatTime.js
@@ -1,1 +1,13 @@
-export const formatTime = () => 'formatted time';
+export const formatTime = (seconds) => {
+  if(!seconds || isNaN(seconds) || seconds < 0){
+    return null
+  }else{
+    const sec = Math.floor(seconds%60);
+    const min = Math.floor((seconds/60)%60);
+    const hrs = Math.floor(seconds/3600);
+    const formatNum = (num) => String(num).padStart(2, '0');
+
+    return formatNum(hrs) + ':' + formatNum(min) + ':' + formatNum(sec);
+  }
+};
+
---
+++ src/utils/utils.test.js
@@ -0,0 +1,26 @@
+import { formatTime } from './formatTime';
+
+describe('utils', () => {
+  describe('formatTime', () => {
+
+    it('should return null if there is no arg', () => {
+      expect(formatTime()).toBe(null);
+    });
+
+    it('should return null if arg is NaN', () => {
+      expect(formatTime(NaN)).toBe(null);
+    });
+
+    it('should return null if arg is lower than 0', () => {
+      expect(formatTime(-1)).toBe(null);
+    });
+
+    it('should return time in hh:mm:ss if arg is proper', () => {
+      expect(formatTime(122)).toBe('00:02:02');
+      expect(formatTime(3793)).toBe('01:03:13');
+      expect(formatTime(120)).toBe('00:02:00');
+      expect(formatTime(3604)).toBe('01:00:04');
+    });
+
+  });
+});
